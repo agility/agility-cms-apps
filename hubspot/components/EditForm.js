60e9ab0a7e876c310964dd6994ebc126824c7ab9
Jerome <jerome@agilitycms.com>
2022-01-24 13:46:45 -0500

    update: Add and edit form components

---
+++ hubspot/components/EditForm.js
@@ -0,0 +1,19 @@
+import { APP_CONFIG } from "../common/config";
+
+const EditForm = ({ form }) => {
+  if (!form) return null;
+  const {guid, portalId} = form;
+  const editUrl = `${APP_CONFIG.DASHBOARD_ENDPOINT}/${portalId}/editor/${guid}/edit/form`;
+  return (
+    <a 
+      href={editUrl} 
+      target="_blank" 
+      rel="noopener, noreferrer" 
+      className="btn btn-primary edit-btn">
+        Edit Form
+    </a>
+  );
+  
+};
+
+export default EditForm;
