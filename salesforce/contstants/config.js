34f6072eb9fd22142950db4e181c2f96eeb6ab45
Jerome <jerome@agilitycms.com>
2022-01-28 16:03:29 -0500

    add: init files 2

---
+++ salesforce/contstants/config.js
@@ -0,0 +1,5 @@
+export const APP_CONFIG = {
+    NAME: "Hubspot",
+    VERSION: "1.0.0",
+    DOCUMENTATION: "https://agilitycms.com/docs/developers/hubspot",
+}
---
+++ salesforce/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    colors: {
+      cta: '#4600a8',
+      ctaHover: '#5800d4',
+      white: '#fff'
+    },
+    fontFamily: {
+      muli: ['Muli', 'Helvetica', 'Arial', 'sans']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
