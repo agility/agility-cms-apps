d3c9f8239f7e463a8eb6fbbefa88c22f49a647d2
Jerome <jerome@agilitycms.com>
2022-02-02 11:23:49 -0500

    update: refactor default form data

--- salesforce/src/components/SalesforceForm/SalesforceForm.tsx
+++ salesforce/src/components/SalesforceForm/SalesforceForm.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState, useRef, useCallback } from "react";
 import agilityAppSDK from "@agility/app-sdk";
 import { ReactSortable } from "react-sortablejs";
-import { MenuIcon } from '@heroicons/react/solid'
+import { MenuIcon, InformationCircleIcon } from '@heroicons/react/solid'
 
 import { useForm } from "../../hooks/useForm";
+
+interface FieldConfig {
+  label?: string,
+  required?: boolean,
+  description?: string
+}
 
 export const SalesforceForm = (): JSX.Element => {
   // set up state
   const [value, setValue] = useState('');
-  const [fieldConfig, setFieldConfig] = useState(null);
+  const [fieldConfig, setFieldConfig] = useState<FieldConfig | null>(null);
   const [configValues, setConfigValues] = useState({});
   const [sdk, setSDK] = useState<any>({});
   const [form, setForm, updateForms] = useForm();
@@ -28,43 +34,49 @@ export const SalesforceForm = (): JSX.Element => {
 
   useEffect(() => {
     // fetch data saved by sdk and populate the form builder
-    if(value) {
-      setForm(JSON.parse(value));
-    }
+    if(value) setForm(JSON.parse(value))
   }, [value, setForm]);
 
   const handleChange = useCallback((id: string) => {
     // update the form with the selected item and save
     updateForms(id)
     sdk.updateFieldValue({ fieldValue: JSON.stringify(form) });
-  }, [form, sdk, updateForms]); 
+  }, [form, sdk, updateForms]);
 
-  const handleArrange = () => {
+  const handleReorder = () => {
     // update the form with the re-arranged list
     setForm(form);
     sdk.updateFieldValue({ fieldValue: JSON.stringify(form) });
-  }; 
+  };
 
   return (
     <div className="field-row" ref={containerRef}>
+      <label className="flex text-sm">
+        <span>{fieldConfig?.label}</span>
+        {fieldConfig?.required && <span className="pl-1 text-red-500">*</span>}
+        {fieldConfig?.description && 
+          <small className="inline" title={fieldConfig?.description}>
+            <InformationCircleIcon className="inline h-4 text-gray-500" />
+          </small>
+        }
+      </label>
       <fieldset>
-        <legend className="text-lg font-medium text-gray-900">Form Builder</legend>
-        <div className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200 max-h-96 overflow-y-auto">
-          <ReactSortable list={form} setList={setForm} handle=".sortButton" onEnd={handleArrange}>
+        <div className="mt-2 overflow-y-auto border-t border-b border-gray-200 divide-y divide-gray-200 max-h-96">
+          <ReactSortable list={form} setList={setForm} handle=".sortButton" onEnd={handleReorder}>
             {form.map((field) => (
               <div key={field.id} className="relative flex items-start py-3 pr-5">
-                <div className="min-w-0 flex-1 text-sm">
-                  <label htmlFor={`field-${field.id}`} className="font-medium text-gray-700 select-none flex">
-                    <MenuIcon className="h-5 mr-3 cursor-pointer sortButton" />
+                <div className="flex-1 min-w-0 text-sm">
+                  <label htmlFor={`field-${field.id}`} className="flex font-medium text-gray-700 select-none">
+                   <MenuIcon className="h-5 mr-3 cursor-move sortButton" />
                     {field.name}
                   </label>
                 </div>
-                <div className="ml-3 flex items-center h-5">
+                <div className="flex items-center h-5 ml-3">
                   <input
                     id={`field-${field.id}`}
                     name={`field-${field.id}`}
                     type="checkbox"
-                    className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                    className="w-4 h-4 text-indigo-600 border-gray-300 rounded cursor-pointer focus:ring-indigo-500"
                     disabled={field.isDefault}
                     checked={field.isSelected}
                     onChange={() => handleChange(field.id)}
