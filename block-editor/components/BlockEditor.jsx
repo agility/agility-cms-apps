f1cc0a5a1ccce23b0b4a75d00981937a14392700
Joel Varty <joel@agilitycms.com>
2022-03-09 10:56:14 -0500

    fix to multi block

--- block-editor/components/BlockEditor.jsx
+++ block-editor/components/BlockEditor.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react"
-import agilityAppSDK from '@agility/app-sdk'
 
+import agilityAppSDK from '@agility/app-sdk'
+import useOnScreen from "../hooks/useOnScreen"
 import EditorJS from '@editorjs/editorjs'
 import Embed from '@editorjs/embed'
 import Table from '@editorjs/table'
@@ -23,104 +24,121 @@ import DragDrop from 'editorjs-drag-drop'
 
 
 const BlockEditor = ({ appConfig }) => {
-    
-	const containerRef = useRef()
+
+    const containerRef = useRef()
+    const blockRef = useRef()
     const [editor, setEditor] = useState({})
+    const isVisible = useOnScreen(containerRef)
+
+    const [fieldSDK, setFieldSDK] = useState(null)
 
     useEffect(() => {
-        //if running locally after a hot-module replacement, don't reinitialize everything...
-        if(editor && Object.keys(editor).length > 0) return;
-        
+
         //get the field ready to wait for messages from the parent
         agilityAppSDK.initializeField({ containerRef }).then((fieldSDK) => {
-            
-            const uploadImagePayload = {
-                websiteName: fieldSDK.websiteName,
-                securityKey: fieldSDK.configValues.securityKey,
-                location: fieldSDK.configValues.dcLocation,
-                assetFolder: fieldSDK.configValues.assetFolder ? fieldSDK.configValues.assetFolder : ''
-            };
+            setFieldSDK(fieldSDK)
 
-            console.log(fieldSDK)
-            
-            const valueJS = fieldSDK.field.value ? JSON.parse(fieldSDK.field.value) : null;
-            
-            const editorJS = new EditorJS({
-                autofocus: false, //setting this to true will not do anything because this is in an iframe
-                holder: document.querySelector('#editorjs'),
-                placeholder: "📝 Enter text, paste images/embed urls, or select a block to add here...",
-                tools:{
-                    table: Table,
-                    paragraph: {
-                        class: Paragraph,
-                        inlineToolbar: true,
-                    },
-                    list: {
-                        class: NestedList,
-                        inlineToolbar: true
-                    },
-                    warning: Warning,
-                    code: Code,
-                    image: {
-                        class: Image,
-                        config: {
-                            endpoints: {
-                                byFile: '/api/image/uploadByFile',
-                                byUrl: '/api/image/fetchByUrl'
-                            },
-                            additionalRequestData: {...uploadImagePayload}
-                        }
-                    },
-                    raw: Raw,
-                    header: Header,
-                    quote: Quote,
-                    marker: Marker,
-                    delimiter: Delimiter,
-                    inlineCode: InlineCode,
-                    embed: Embed,
-                    //enable this if you have env-vars set
-                    // link: {
-                    //     class: LinkAutocomplete,
-                    //     config: {
-                    //         endpoint: '/api/link/search',
-                    //         queryParam: 'q'
-                    //     }
-                    // }
-                },
-                onChange: () => {
-        
-                    editorJS.save().then(outputValue => {
-                        const valueJSON = JSON.stringify(outputValue)
-                        fieldSDK.updateFieldValue({ fieldValue: valueJSON })
-                    })
-        
-                },
-                onReady: () => {
-                    new DragDrop(editorJS);
-                    //const undo = new Undo({editorJS})
-        
-                    if(valueJS && valueJS.blocks && valueJS.blocks.length > 0) {
-                        editorJS.render(valueJS);
-                        //undo.initialize(fieldValue);
-                    }
-                
-                }
-        
-            });
-
-            setEditor(editorJS);
         });
-    }, [appConfig, editor]);
+    }, [appConfig, editor, isVisible]);
 
-	return (
-		<div style={{ background: "#fff", padding: '0 10px' }}>
-            <span style={{ fontSize: '12px', background: 'rgb(251 230 171 / 48%)', color: '#fb8b00', borderRadius: '5px', padding: '3px 4px', display: 'inline-block', fontWeight: '500'}}>Block Editor (Experimental)</span>
-			<div id="editorjs" ref={containerRef}>
+    useEffect(() => {
+        if (!isVisible || !fieldSDK) return
 
-			</div>
-		</div>
+        //if running locally after a hot-module replacement, don't reinitialize everything...
+        if (editor && Object.keys(editor).length > 0) return;
 
-	);
+        const uploadImagePayload = {
+            websiteName: fieldSDK.websiteName,
+            securityKey: fieldSDK.configValues.securityKey,
+            location: fieldSDK.configValues.dcLocation,
+            assetFolder: fieldSDK.configValues.assetFolder ? fieldSDK.configValues.assetFolder : ''
+        };
+
+        const valueJS = fieldSDK.field.value ? JSON.parse(fieldSDK.field.value) : null;
+
+        const editorJS = new EditorJS({
+            autofocus: false, //setting this to true will not do anything because this is in an iframe
+            holder: blockRef.current,
+            placeholder: "📝 Enter text, paste images/embed urls, or select a block to add here...",
+            tools: {
+                table: Table,
+                paragraph: {
+                    class: Paragraph,
+                    inlineToolbar: true,
+                },
+                list: {
+                    class: NestedList,
+                    inlineToolbar: true
+                },
+                warning: Warning,
+                code: Code,
+                image: {
+                    class: Image,
+                    config: {
+                        endpoints: {
+                            byFile: '/api/image/uploadByFile',
+                            byUrl: '/api/image/fetchByUrl'
+                        },
+                        additionalRequestData: { ...uploadImagePayload }
+                    }
+                },
+                raw: Raw,
+                header: Header,
+                quote: Quote,
+                marker: Marker,
+                delimiter: Delimiter,
+                inlineCode: InlineCode,
+                embed: Embed,
+                //enable this if you have env-vars set
+                // link: {
+                //     class: LinkAutocomplete,
+                //     config: {
+                //         endpoint: '/api/link/search',
+                //         queryParam: 'q'
+                //     }
+                // }
+            },
+            onChange: (e) => {
+                console.log(e)
+                editorJS.save().then(outputValue => {
+
+                    const valueJSON = JSON.stringify(outputValue)
+
+                    console.log(valueJSON)
+
+                    fieldSDK.updateFieldValue({ fieldValue: valueJSON })
+                })
+
+            },
+            onReady: () => {
+
+                new DragDrop(editorJS);
+                //const undo = new Undo({editorJS})
+
+                if (valueJS && valueJS.blocks && valueJS.blocks.length > 0) {
+                    editorJS.render(valueJS);
+                    //undo.initialize(fieldValue);
+                }
+
+            }
+
+        });
+
+        setEditor(editorJS);
+
+    }, [isVisible, fieldSDK, editor])
+
+    return (
+        <div style={{ background: "#fff", padding: '0 10px', minHeight: "50px" }} ref={containerRef}>
+            <div style={{ fontSize: '12px', background: '#ebebeb', borderRadius: 5, color: '#999', borderRadius: '3px', padding: '5px 21px', marginBottom: "5px", fontWeight: '500' }}>Block Editor (Beta)</div>
+
+            <div ref={blockRef}>
+
+            </div>
+
+        </div>
+
+    );
 
 }
 
