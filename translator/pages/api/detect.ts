59e617468f453014948a2211edb9c1c5358f6e2b
Joel Varty <joel@agilitycms.com>
2022-03-31 16:02:17 -0400

    removed replaceAll

--- translator/pages/api/detect.ts
+++ translator/pages/api/detect.ts
@@ -14,7 +14,7 @@ export default async function detect(req: NextApiRequest, res: NextApiResponse<D
 	const projectId = req.body.configValues.google_projectID
 	const client_email = req.body.configValues.google_client_email
 	let private_key:string = req.body.configValues.google_private_key
-	private_key = private_key.replaceAll("\\n", "\n")
+	private_key = private_key.replace(/\\n/, "\n")
 
 	const translate = new Translate({
 		projectId,
