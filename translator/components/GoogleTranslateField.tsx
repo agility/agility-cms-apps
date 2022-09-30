a9cad55069ded28e221e7ea586f5194cd8a7b2a3
Dakotah <dakotah.godfrey.dev>
2022-09-30 18:47:38 -0400

    error handling for user config data

--- translator/components/GoogleTranslateField.tsx
+++ translator/components/GoogleTranslateField.tsx
@@ -1,217 +1,255 @@
-import {useEffect, useRef, useState} from "react"
-import agilityAppSDK from "@agility/app-sdk"
-import axios from "axios"
-import languages from "../data/locales.json"
+import { useEffect, useRef, useState } from "react";
+import agilityAppSDK from "@agility/app-sdk";
+import axios from "axios";
+import languages from "../data/locales.json";
 
 interface FieldConfig {
-	label?: string
-	required?: boolean
-	description?: string
-	readOnly?: boolean
+  label?: string;
+  required?: boolean;
+  description?: string;
+  readOnly?: boolean;
 }
 
 interface ContentItem {
-	contentID: number
-	referenceName: string
-	values: {[id: string]: any}
+  contentID: number;
+  referenceName: string;
+  values: { [id: string]: any };
 }
 
 interface ContentField {
-	label: string
-	name: string
-	dataField: boolean
-	description: string
-	designerOnly: boolean
-	editable: boolean
-	fieldType: string
-	hidden: boolean
-	itemOrder: number
+  label: string;
+  name: string;
+  dataField: boolean;
+  description: string;
+  designerOnly: boolean;
+  editable: boolean;
+  fieldType: string;
+  hidden: boolean;
+  itemOrder: number;
 }
 
 interface ContentModel {
-	modelID: number
-	title: string
-	referenceName: string
-	fields: [ContentField]
+  modelID: number;
+  title: string;
+  referenceName: string;
+  fields: [ContentField];
 }
 
 export default function GoogleTranslateField() {
-	const [value, setValue] = useState("")
-	const [contentItem, setContentItem] = useState<ContentItem | null>(null)
-	const [contentModel, setContentModel] = useState<ContentModel | null>(null)
-	const [fieldConfig, setFieldConfig] = useState<FieldConfig | null>(null)
-	const [configValues, setConfigValues] = useState<{leadOID: string} | null>(null)
-	const [sdk, setSDK] = useState<any>({})
-	const [locale, setLocale] = useState<any>(null)
-	const [processing, setProcessing] = useState(false)
+  const [value, setValue] = useState("");
+  const [contentItem, setContentItem] = useState<ContentItem | null>(null);
+  const [contentModel, setContentModel] = useState<ContentModel | null>(null);
+  const [fieldConfig, setFieldConfig] = useState<FieldConfig | null>(null);
+  const [configValues, setConfigValues] = useState<{ leadOID: string } | null>(
+    null
+  );
+  const [sdk, setSDK] = useState<any>({});
+  const [locale, setLocale] = useState<any>(null);
+  const [processing, setProcessing] = useState(false);
 
-	const [errorMsg, setErrorMsg] = useState<string | null>(null)
-	const [msg, setMsg] = useState<string | null>(null)
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [msg, setMsg] = useState<string | null>(null);
 
-	const [detected, setDetected] = useState(false)
-	const [needsTranslation, setNeedsTranslation] = useState(false)
+  const [detected, setDetected] = useState(false);
+  const [needsTranslation, setNeedsTranslation] = useState(false);
 
-	// set ref
-	const containerRef = useRef(null)
+  // set ref
+  const containerRef = useRef(null);
 
-	// initialize field
-	useEffect(() => {
-		agilityAppSDK.initializeField({containerRef}).then((fieldSDK: any) => {
-			setSDK(fieldSDK)
-			setValue(fieldSDK.field.value)
-			setConfigValues(fieldSDK.configValues)
-			setFieldConfig(fieldSDK.field)
+  // initialize field
+  useEffect(() => {
+    agilityAppSDK.initializeField({ containerRef }).then((fieldSDK: any) => {
+      setSDK(fieldSDK);
+      setValue(fieldSDK.field.value);
+      setConfigValues(fieldSDK.configValues);
+      setFieldConfig(fieldSDK.field);
 
-			setContentItem(fieldSDK.contentItem)
-			setContentModel(fieldSDK.contentModel)
+      setContentItem(fieldSDK.contentItem);
+      setContentModel(fieldSDK.contentModel);
 
-			let languageCode: string = fieldSDK.locale
-			if (languageCode.length > 2) languageCode = languageCode.substring(0, 2).toLowerCase()
+      let languageCode: string = fieldSDK.locale;
+      if (languageCode.length > 2)
+        languageCode = languageCode.substring(0, 2).toLowerCase();
 
-			changeLocale(languageCode)
+      changeLocale(languageCode);
 
-			//on load, we should be checking the language of the content against the current language
-			// and HIDE this if we do NOT need to translate it
-			detectCurrentLanguage(fieldSDK, languageCode)
-		})
+      //on load, we should be checking the language of the content against the current language
+      // and HIDE this if we do NOT need to translate it
+      detectCurrentLanguage(fieldSDK, languageCode);
+    });
 
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	const changeLocale = (languageCode: string) => {
-		if (!languageCode) {
-			setLocale(null)
-		} else {
-			const theLocale = languages.find((l) => l.code === languageCode)
-			if (theLocale) setLocale(theLocale)
-		}
-	}
+  const changeLocale = (languageCode: string) => {
+    if (!languageCode) {
+      setLocale(null);
+    } else {
+      const theLocale = languages.find((l) => l.code === languageCode);
+      if (theLocale) setLocale(theLocale);
+    }
+  };
 
+  const detectCurrentLanguage = async (fieldSDK: any, languageCode: string) => {
+    try {
+      const item: ContentItem = fieldSDK.contentItem;
 
-	const detectCurrentLanguage = async (fieldSDK: any, languageCode: string) => {
+      if (item.contentID < 1) return;
 
-		try {
-			const item: ContentItem = fieldSDK.contentItem
+      const model: ContentModel = fieldSDK.contentModel;
+      let body: any = {
+        configValues: fieldSDK.configValues,
+        textValues: {},
+      };
 
-			if (item.contentID < 1) return
+      //loop all the field fields and pull out the text or html that are NOT hidden
+      model?.fields
+        .filter(
+          (f) =>
+            f.dataField &&
+            !f.hidden &&
+            (f.fieldType === "Text" || f.fieldType === "HTML")
+        )
+        .forEach((field) => {
+          body.textValues[field.name] = item.values[field.name];
+        });
 
-			const model:ContentModel = fieldSDK.contentModel
-			let body: any = {
-				configValues: fieldSDK.configValues,
-				textValues: {},
-			}
+      //send the request to our translation function....
+      const res = await axios.post("/api/detect", body);
 
-			//loop all the field fields and pull out the text or html that are NOT hidden
-			model?.fields
-				.filter((f) => f.dataField && !f.hidden && (f.fieldType === "Text" || f.fieldType === "HTML"))
-				.forEach((field) => {
-					body.textValues[field.name] = item.values[field.name]
-				})
+      if (res.data?.detectedLanguage === null) {
+        setErrorMsg("Unable to detect language");
+      }
+      if (res.data?.detectedLanguage !== languageCode) {
+        setNeedsTranslation(true);
+      } else {
+        console.log(
+          "detected lang: ",
+          res.data?.detectedLanguage,
+          "current lang: ",
+          languageCode
+        );
+      }
+    } catch (error: any) {
+      //TODO: figure out how to show an error message here...
+      console.error("An error occurred while detecting language: ", error);
+      if (error?.response.data.library === "PEM Routines") {
+        setErrorMsg(
+          "Error: Please make sure you're including the entire private key"
+        );
+      }
+      if (error?.response.data.error_description) {
+        //   console.dir(error);
+        setErrorMsg(
+          `${error.response.status}: ${error.response.data.error_description}`
+        );
+      }
+    } finally {
+      setDetected(true);
+    }
+  };
 
-			//send the request to our translation function....
-			const res = await axios.post("/api/detect", body)
+  const translate = async () => {
+    if (!locale) {
+      //TODO: show an error here...
+      setErrorMsg("Please select a locale to translate to.");
+      return;
+    }
 
-			if (res.data?.detectedLanguage !== languageCode) {
-				setNeedsTranslation(true)
-			} else {
-				console.log("detected lang: ", res.data?.detectedLanguage, "current lang: ", languageCode)
-			}
+    setProcessing(true);
+    setErrorMsg(null);
 
-		} catch (error) {
-			//TODO: figure out how to show an error message here...
-			console.error("An error occurred while detecting language: ", error)
-		} finally {
-			setDetected(true)
-		}
-	}
+    try {
+      const item: ContentItem = await sdk.getContentItem();
 
-	const translate = async () => {
-		if (!locale) {
-			//TODO: show an error here...
-			setErrorMsg("Please select a locale to translate to.")
-			return
-		}
+      let body: any = {
+        locale: locale.code,
+        configValues,
+        textValues: {},
+      };
 
-		setProcessing(true)
-		setErrorMsg(null)
+      //loop all the field fields and pull out the text or html that are NOT hidden
+      contentModel?.fields
+        .filter(
+          (f) =>
+            f.dataField &&
+            !f.hidden &&
+            (f.fieldType === "Text" || f.fieldType === "HTML")
+        )
+        .forEach((field) => {
+          body.textValues[field.name] = item.values[field.name];
+        });
 
-		try {
-			const item: ContentItem = await sdk.getContentItem()
+      //send the request to our translation function....
+      const res = await axios.post("/api/translate", body);
+      let fieldsUpdateCount = 0;
+      for (let fieldName in res.data.values) {
+        const fieldValue = res.data.values[fieldName];
 
-			let body: any = {
-				locale: locale.code,
-				configValues,
-				textValues: {},
-			}
+        if (fieldValue) {
+          sdk.updateFieldValue({ fieldName, fieldValue });
+          fieldsUpdateCount++;
+        }
+      }
 
-			//loop all the field fields and pull out the text or html that are NOT hidden
-			contentModel?.fields
-				.filter((f) => f.dataField && !f.hidden && (f.fieldType === "Text" || f.fieldType === "HTML"))
-				.forEach((field) => {
-					body.textValues[field.name] = item.values[field.name]
-				})
+      setMsg(`${fieldsUpdateCount} field(s) updated.`);
+    } catch (error) {
+      setErrorMsg("An error occurred while translating.");
+      console.error("An error occurred while translating: ", error);
+    } finally {
+      setProcessing(false);
+    }
+  };
 
-			//send the request to our translation function....
-			const res = await axios.post("/api/translate", body)
-			let fieldsUpdateCount = 0
-			for (let fieldName in res.data.values) {
-				const fieldValue = res.data.values[fieldName]
-
-				if (fieldValue) {
-					sdk.updateFieldValue({ fieldName, fieldValue })
-					fieldsUpdateCount++
-				}
-			}
-
-			setMsg(`${fieldsUpdateCount} field(s) updated.`)
-
-		} catch (error) {
-			setErrorMsg("An error occurred while translating.")
-			console.error("An error occurred while translating: ", error)
-		} finally {
-			setProcessing(false)
-		}
-	}
-
-	return (
-		<div ref={containerRef}>
-			{(contentItem?.contentID || 0) > 0 && !detected && <div className="text-xs">Detecting current language...</div>}
-			{(contentItem?.contentID || 0) > 0 && needsTranslation && (
-				<div className="border border-gray-300 rounded p-3">
-					<div className="flex items-center justify-between pb-2">
-						<div>GOOGLE TRANSLATE</div>
-						{errorMsg && <div className="bg-red-200 text-red-600 text-xs rounded py-1 px-2">{errorMsg} </div>}
-						{!errorMsg && msg && <div className="bg-blue-200 text-blue-600 text-xs rounded py-1 px-2">{msg} </div>}
-					</div>
-					<div className="flex items-center">
-						<div className="flex-1">
-							Click to translate the text values in this item to&nbsp;
-							<select
-								className="bg-blue-100 rounded-xl px-2"
-								value={locale?.code}
-								onChange={(e) => changeLocale(e.target.value)}
-							>
-								<option value="">Select Locale</option>
-								{languages.map((lang) => (
-									<option key={lang.code} value={lang.code}>
-										{lang.name}
-									</option>
-								))}
-							</select>
-						</div>
-						<div>
-							<button
-								onClick={() => translate()}
-								className="rounded-md w-48 bg-gray-200 py-1 px-3 hover:bg-gray-300 focus:bg-gray-400 disabled:bg-gray-100 disabled:text-gray-400"
-								disabled={processing}
-							>
-								{!processing && "Translate"}
-								{processing && "Processing..."}
-							</button>
-						</div>
-					</div>
-				</div>
-			)}
-		</div>
-	)
+  return (
+    <div ref={containerRef}>
+      {(contentItem?.contentID || 0) > 0 && !detected && (
+        <div className='text-xs'>Detecting current language...</div>
+      )}
+      {(contentItem?.contentID || 0) > 0 && (
+        <div className='border border-gray-300 rounded p-3'>
+          <div className='flex items-center justify-between pb-2'>
+            <div>GOOGLE TRANSLATE</div>
+            {errorMsg && (
+              <div className='bg-red-200 text-red-600 text-xs rounded py-1 px-2'>
+                {errorMsg}{" "}
+              </div>
+            )}
+            {!errorMsg && msg && (
+              <div className='bg-blue-200 text-blue-600 text-xs rounded py-1 px-2'>
+                {msg}{" "}
+              </div>
+            )}
+          </div>
+          <div className='flex items-center'>
+            <div className='flex-1'>
+              Click to translate the text values in this item to&nbsp;
+              <select
+                className='bg-blue-100 rounded-xl px-2'
+                value={locale?.code}
+                onChange={(e) => changeLocale(e.target.value)}
+              >
+                <option value=''>Select Locale</option>
+                {languages.map((lang) => (
+                  <option key={lang.code} value={lang.code}>
+                    {lang.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <button
+                onClick={() => translate()}
+                className='rounded-md w-48 bg-gray-200 py-1 px-3 hover:bg-gray-300 focus:bg-gray-400 disabled:bg-gray-100 disabled:text-gray-400'
+                disabled={processing || !needsTranslation}
+              >
+                {!processing && "Translate"}
+                {processing && "Processing..."}
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
--- translator/pages/api/detect.ts
+++ translator/pages/api/detect.ts
@@ -1,51 +1,58 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-import { v2 } from "@google-cloud/translate"
-const Translate = v2.Translate
-
+import { v2 } from "@google-cloud/translate";
+const Translate = v2.Translate;
 
 type Data = {
-	detectedLanguage: string | null
-}
+  detectedLanguage: string | null;
+};
 
-export default async function detect(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function detect(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | Error>
+) {
+  const projectId = req.body.configValues.google_projectID;
+  const client_email = req.body.configValues.google_client_email;
+  let private_key: string = req.body.configValues.google_private_key;
+  private_key = private_key.replace(/\\n/g, "\n");
 
-	const projectId = req.body.configValues.google_projectID
-	const client_email = req.body.configValues.google_client_email
-	let private_key:string = req.body.configValues.google_private_key
-	private_key = private_key.replace(/\\n/g, "\n")
+  const translate = new Translate({
+    projectId,
+    credentials: {
+      private_key,
+      client_email,
+    },
+  });
 
-	const translate = new Translate({
-		projectId,
-		credentials: {
-			private_key,
-			client_email
-		}
-	});
+  // // Translates some text into Russian
+  let detectedLanguage: string | null = null;
+  for (let key in req.body.textValues) {
+    const valueToTranslate = req.body.textValues[key];
 
+    try {
+      const value = await translate.detect(valueToTranslate);
 
-	// // Translates some text into Russian
-	let detectedLanguage:string|null = null
-	for (let key in req.body.textValues) {
-		const valueToTranslate = req.body.textValues[key]
+      if (value[0].confidence === 1) {
+        detectedLanguage = value[0].language;
+        break;
+      }
+    } catch (error: any) {
+      // console.log("COULD NOT detect language", key, error);
+      if (error instanceof Error) {
+        if (error.message.includes("PEM routines")) {
+          res.status(403).json(error);
+          return;
+        }
+      }
+      if (error?.response.status) {
+        res.status(error.response.status).json(error.response.data);
+        return;
+      }
+      res.status(500).json(error);
+      return;
+    }
+  }
 
-		try {
-			const value = await translate.detect(valueToTranslate);
-
-			if (value[0].confidence === 1) {
-				detectedLanguage = value[0].language
-				break
-			}
-
-		} catch (error) {
-			console.log("COULD NOT detect language", key, error)
-		}
-	}
-
-
-
-	res.status(200).json({ detectedLanguage })
-
-
+  res.status(200).json({ detectedLanguage });
 }
