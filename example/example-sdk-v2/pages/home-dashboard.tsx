78ff97f9c9894650b45fb312b274706799f478ab
Joel Varty <joel@agilitycms.com>
2023-04-11 16:03:11 -0400

    update to example

--- example/example-sdk-v2/pages/home-dashboard.tsx
+++ example/example-sdk-v2/pages/home-dashboard.tsx
@@ -1,4 +1,15 @@
 
+import { useAgilityAppSDK } from '@agility/app-sdk'
+
 export default function HomeDashboard() {
-	return <div>Example App - Home Dashboard</div>
+
+	const {  initializing, appInstallContext, instance, locale} = useAgilityAppSDK()
+
+	return (
+		<div>
+			<h1>Example App - Home Dashboard</h1>
+			<div>Initializing: {initializing.toString()}</div>
+			<div>Locale: {locale}</div>
+		</div>
+	)
 }
